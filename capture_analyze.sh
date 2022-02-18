739434746fb8accbd382adde4fc5983d34dc6721
tommyjlong <tommylong@bellsouth.net>
2022-02-18 17:57:37 -0500

    make IP address consistent with documentation

--- capture_analyze.sh
+++ capture_analyze.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "Capturing Image (using curl)"
-curl --max-time 60 -X GET http://192.168.0.10:8080/api/capture_image
+curl --max-time 60 -X GET http://192.168.0.2:8080/api/capture_image
 echo ""
 sleep 1
 
 #If running in a python virtual environment:
 echo "Sourcing venv"
-source /opt/homeassistant/venv_3.8/bin/activate
+source /opt/gasmeter/venv_3.8/bin/activate
 
 #Run the Analyzer
 echo "Running Analyzer"
-python /opt/homeassistant/venv_3.8/gasmeter_analyzer.py
+python /opt/gasmeter/venv_3.8/gasmeter_analyzer.py
